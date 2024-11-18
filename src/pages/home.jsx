37562acc97cde1b8a9ee1d2c13fc7d442882aafd
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-11-18 13:20:51 +0000

    fixed compiling and initiation issues

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Navbar from "./Components/Navbar";
+import Navbar from '../components/Navbar';
 
 
-const Home = () => {
+function Home() {
 return (
     <>
         <Navbar />
