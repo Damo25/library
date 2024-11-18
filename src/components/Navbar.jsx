2ed8a9b1971afa3b058314c232eef1bcce13716c
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-11-18 12:16:38 +0000

    added content to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,28 @@
 import React from 'react';
+
+const Navbar = ({
+
+}) => {
+    return (
+
+<div>
+      <nav className="bg-gray-800 w-screen">
+        <div className="flex justify-between items-center h-20 px-6">
+          <div className="flex items-center">
+
+            
+            <a href="/App" className="text-white">
+              Library
+            </a>
+          </div>
+
+          <div className="flex rounded-md mt-2">
+            
+          </div>
+        </div>
+      </nav>
+    </div>
+    );
+};
+
+export default Navbar;
