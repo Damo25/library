e8b660ef43afff67edc35923693eb92f13c7aff9
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-11-19 15:50:24 +0000

    fixed app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import Logout from "./pages/Logout";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Route path="/" element={<Navigate to="/signup" />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/Home" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Admin" element={<Admin />} />
+      <Route path="/Logout" element={<Logout />} />
       </Routes>
     </Router>
   );
