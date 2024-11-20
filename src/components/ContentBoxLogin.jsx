cbefceed3497fadbccd3a0b43ebe1f65bee905a2
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-11-20 19:07:53 +0000

    added form to login

--- src/components/ContentBoxLogin.jsx
+++ src/components/ContentBoxLogin.jsx
@@ -8,6 +8,17 @@ const ContentBoxLogin = () => {
     <div className='box-container-login'>
       <div className='content-box-login'>
         <h1>Login</h1> 
+        <form className="login-form">
+          <div className="form-group">
+            <label htmlFor="username">Username or Email:</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username or email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+          </div>
+          <button type="submit" className="login-button">Log In</button>
+        </form>
     </div>
   </div>
   );
