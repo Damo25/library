104e332b7816efeb0ccb28d29e95c3a8e5cf7158
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-11-25 13:41:20 +0000

    search bar added to admin page

--- src/components/ContentBoxAdmin.jsx
+++ src/components/ContentBoxAdmin.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Seachbar from './SearchBar';
 
 
 const ContentBoxAdmin = () => {
@@ -8,6 +9,7 @@ const ContentBoxAdmin = () => {
     <div className='box-container-register'>
       <div className='content-box-register'>
         <h1>Admin</h1> 
+        <Seachbar />
     </div>
   </div>
   );
