3cb64cc55aff1ba16371fe3af35c46b014b2f689
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-11-26 13:14:15 +0000

    Added to logout page as well as adding styling and added router DOM availabilty to return to home page once logged out.

---
+++ src/components/ContentBoxLogout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ContentBoxLogout = () => {
+
+
+  return (
+    <div className='box-container-logout'>
+      <div className='content-box-logout'>
+        <h1>Are you sure you want to log out?</h1> 
+      <div className='logout-button-spacing'>
+        <Link to="/home" className="logout-button">
+          Log Out
+        </Link>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default ContentBoxLogout;
--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ContentBoxLogout from '../components/ContentBoxLogout';
 
 
 function Logout() {
 return (
     <>
         <Navbar />
+        <ContentBoxLogout />
         <Footer />
     </>
 );
