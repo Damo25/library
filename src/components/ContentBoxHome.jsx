aa6be932fa638d486dbe215befd13a101ce887bc
JohnWebb26 <c1019223@hallam.shu.ac.uk>
2024-12-12 18:58:46 +0000

    styling and formatting changes

--- src/components/ContentBoxHome.jsx
+++ src/components/ContentBoxHome.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import ContentBoxHomeResults from './ContentBoxHomeResults';
+import NavbarHome from './NavbarHome';
 
 const ContentBox = () => {
 
 
   return (
     <div className='box-container'>
-      <div className='content-box align'>
-        <h1>Home</h1> 
+    <div className='content-box align'>
+      <div className='results-padding'>
+      <NavbarHome /> 
+      </div> 
+      <ContentBoxHomeResults />
+      <div className='flex pt-1'>
         <div>
         <SearchBar />
         </div>
-      <div className='s-b-padding'>
-      <button type="submit" className="login-button">Search</button>
+      <div className='pl-2 pt-0.5'>
+        <button type="submit" className="login-button">Search</button></div>
       </div>
-      <div className='pt-5'>
-      <ContentBoxHomeResults />
-      </div>
-      
-    </div>
   </div>
+</div>
   );
 };
 
---
+++ src/components/NavbarHome.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const NavbarHome = () => {
+
+
+  return (
+      <div className="navbarAdmin tranclucent">
+      <div className="navbar-link">
+          Library Books
+        </div>
+      </div>
+  );
+};
+
+export default NavbarHome;
