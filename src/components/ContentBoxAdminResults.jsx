da739306035cb87627618c1ecd15349ce3344aa0
Damo25 <c2003739@hallam.shu.ac.uk>
2024-12-12 17:07:05 +0000

    admin pages changed

--- src/components/ContentBoxAdminResults.jsx
+++ src/components/ContentBoxAdminResults.jsx
@@ -3,19 +3,48 @@ import React from 'react';
 
 const ContentBoxAdminResults = () => {
 
+  const Library = [
+    {
+      id: 1,
+      title: 'Sheffild Hallam Adsetts',
+      location: 'sheffield',
+      image: '/Images/Adsetts.jpg',
+      DaysOpen:"Monday-Sunday",
+    },
+    {
+      id: 2,
+      title: 'Leeds Central',
+      location: 'Leeds City Centre',
+      image: '/Images/exterior-of-leeds-central.jpg',
+      DaysOpen:"Monday-Sunday",
+    },
+  
+ 
+
+  ]
+  
+
 
   return (
-    <div className='results-container'>
-        <h2>Result</h2>
-        <h3>User</h3>
-        <span>Name:</span>
-        <span> Age:</span>
-        <span>Library:</span>
-        <span>Email:</span>
-        <h3>Library</h3>
-        <span>Location</span>
-        <span>Opening Times:</span>
-  </div>
+    <div className="results-container-home p-4 max-h-[80vh] overflow-y-auto">
+      <h2 className="text-2xl font-bold mb-4">Library</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {Library.map((librarys) => (
+          <div key={librarys.id}>
+            <img 
+              src={librarys.image} 
+              alt={`Cover of ${librarys.title}`} 
+              className="w-full h-48 object-cover mb-4 rounded"
+            />
+            <h3 className="text-xl font-semibold mb-2">{librarys.location}</h3>
+            <p className="text-gray-700">Opening Days: {librarys.DaysOpen}</p>
+            <div className='s-b-padding'>
+        <button type="submit" className="login-button">Edit</button>
+        </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
