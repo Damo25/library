05207f3dbdab8123131eb95f9418321ae821a11b
Damo25 <c2003739@hallam.shu.ac.uk>
2024-12-12 18:35:00 +0000

    home fix

--- src/components/ContentBoxHome.jsx
+++ src/components/ContentBoxHome.jsx
@@ -7,7 +7,7 @@ const ContentBox = () => {
 
   return (
     <div className='box-container'>
-      <div className='content-box'>
+      <div className='content-box align'>
         <h1>Home</h1> 
         <div>
         <SearchBar />
